5bf34a4ff3a0f2cd65450a238976bbb71e859794
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-26 20:52:54 +0100

    Set up cloud controllers

---
+++ Assets/Scripts/Clouds/CloudCollector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudCollector : MonoBehaviour {
+
+	private void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Cloud" || col.tag == "Deadly") {
+			col.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Assets/Scripts/Clouds/CloudSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] clouds;
+
+	private float distBetweenClouds = 3f;
+	private float minX, maxX;
+	private float lastCloudPosY;
+	private float controlX;
+
+	[SerializeField]
+	private GameObject[] collectables;
+
+	private GameObject player;
+
+	private void Awake () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+}
