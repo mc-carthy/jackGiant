a0e49d6eab680e09269dec85a2f0eb1a827cdfeb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-27 14:50:55 +0100

    Add menu navigation

---
+++ Assets/Scripts/GameControllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void StartGame () {
+		SceneManager.LoadScene ("main", LoadSceneMode.Single);
+	}
+
+	public void GoToHighscore () {
+		SceneManager.LoadScene ("highscore", LoadSceneMode.Single);
+	}
+
+	public void GoToOptions () {
+		SceneManager.LoadScene ("options", LoadSceneMode.Single);
+	}
+
+	public void ToggleSound () {
+
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/GameControllers/OptionsController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour {
+
+	public void GoToMainMenu () {
+		SceneManager.LoadScene ("menu", LoadSceneMode.Single);
+	}
+}
