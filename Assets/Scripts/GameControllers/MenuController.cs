4918049260faceb7720c5a6eca8c536bde954811
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-29 10:27:13 +0100

    Create MusicController using singleton patter

--- Assets/Scripts/GameControllers/MenuController.cs
+++ Assets/Scripts/GameControllers/MenuController.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MenuController : MonoBehaviour {
+
+	[SerializeField]
+	private Button musicBtn;
+
+	[SerializeField]
+	private Sprite[] musicIcons;
+
+	private void Start () {
+		CheckToPlayMusic ();
+	}
 
 	public void StartGame () {
 		GameManager.instance.gameStartedFromMainMenu = true;
@@ -18,10 +29,25 @@ public class MenuController : MonoBehaviour {
 	}
 
 	public void ToggleSound () {
-
+		if (GamePreferences.GetMusicState() == 0) {
+			GamePreferences.SetMusicState (1);
+		} else {
+			GamePreferences.SetMusicState (0);
+		}
+		CheckToPlayMusic ();
 	}
 
 	public void Quit () {
 		Application.Quit ();
+	}
+
+	private void CheckToPlayMusic () {
+		if (GamePreferences.GetMusicState () == 1) {
+			MusicManager.instance.PlayMusic (true);
+			musicBtn.image.sprite = musicIcons [1];
+		} else {
+			MusicManager.instance.PlayMusic (false);
+			musicBtn.image.sprite = musicIcons [0];
+		}
 	}
 }
---
+++ Assets/Scripts/GameControllers/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public static MusicManager instance;
+
+	private AudioSource source;
+
+	private void Awake () {
+		MakeSingleton ();
+		source = GetComponent<AudioSource> ();
+	}
+
+	private void MakeSingleton () {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	public void PlayMusic (bool play) {
+		if (play) {
+			if (!source.isPlaying) {
+				source.Play ();
+			}
+		} else {
+			if (source.isPlaying) {
+				source.Stop ();
+			}
+		}
+	}
+}
